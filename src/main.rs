c14b16cda700496bcef943e62df0d4362a5c9e75
pravin <pravinramana25@protonmail.ch>
2021-09-02 17:28:12 -0500

    parse video titles

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::process::exit;
 
 fn main() {
     let prepend = "https://www.youtube.com/results?search_query=";
-    let query = "shrek memes";
+    let query = "take on me";
     let matcher = Regex::new("var ytInitialData = .*}}}}}}").unwrap();
     let client = Client::new();
     let data = &client
@@ -23,5 +23,35 @@ fn main() {
     };
     let json = json.trim_start_matches("var ytInitialData = ");
     let json: Value = serde_json::from_str(json).unwrap();
-    dbg!(json);
+    let video_data = json
+        .get("contents")
+        .unwrap()
+        .get("twoColumnSearchResultsRenderer")
+        .unwrap()
+        .get("primaryContents")
+        .unwrap()
+        .get("sectionListRenderer")
+        .unwrap()
+        .get("contents")
+        .unwrap()
+        .get(0)
+        .unwrap()
+        .get("itemSectionRenderer")
+        .unwrap()
+        .get("contents")
+        .unwrap();
+    for video_datum in video_data.as_array().unwrap() {
+        if let Some(data) = video_datum.get("videoRenderer") {
+            let title = data
+                .get("title")
+                .unwrap()
+                .get("runs")
+                .unwrap()
+                .get(0)
+                .unwrap()
+                .get("text")
+                .unwrap();
+            dbg!(title);
+        }
+    }
 }
